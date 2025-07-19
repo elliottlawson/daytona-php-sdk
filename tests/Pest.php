241f9682aa7a5bd2d7776bee7a79017984f4f0ad
Elliott Lawson <elliott.lawson@outlook.com>
2025-07-18 22:41:31 -0400

    fixed test suite

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,10 +3,8 @@
 use ElliottLawson\Daytona\Tests\Integration\IntegrationTestCase;
 use ElliottLawson\Daytona\Tests\TestCase;
 
-// Use TestCase for unit tests
 uses(TestCase::class)->in('Feature');
 
-// Use IntegrationTestCase for integration tests
 uses(IntegrationTestCase::class)
     ->group('integration')
     ->in('Integration');
